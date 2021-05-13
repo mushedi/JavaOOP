bc321b32c07cf13bfac6e6ad7fe23d8d2f2612e2
Azul <svcredblu@gmail.com>
2021-05-13 16:40:35 -0500

    beginning program 12

---
+++ program12/src/HourlyEmployee.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+public class HourlyEmployee extends Employee {
+	
+	   public HourlyEmployee(String firstName, String lastName, double payRate)
+	   {
+	      super(firstName, lastName, payRate);
+	   }
+	public static void main(String[] args) {
+		
+	}
+	
+}
